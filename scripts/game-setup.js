e465c8c2aef8fab375a0d34972f477d2735074a6
Alexrand <namelost@yandex.ru>
2018-03-22 19:48:46 +0800

    added basic setup properties;

---
+++ scripts/game-setup.js
@@ -0,0 +1,11 @@
+'use strict';
+(function () {
+	window.gameSetup = {
+		'blockSize': 50,
+		'bricksAmount': 50,
+		'gameFinished': false,
+		'npcTanksAnount': 2,
+		'treesAmount': 10,
+		'wallsAmount': 10
+	};
+})();

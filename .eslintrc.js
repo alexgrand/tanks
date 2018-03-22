677ed5926d71d57cbdbb8bcd784905271a7a3484
Alexrand <namelost@yandex.ru>
2018-03-22 22:19:07 +0800

    rule "sort-keys" off;

--- .eslintrc.js
+++ .eslintrc.js
@@ -222,7 +222,7 @@ module.exports = {
             "last"
         ],
         "sort-imports": "error",
-        "sort-keys": "error",
+        "sort-keys": "off",
         "sort-vars": "error",
         "space-before-blocks": "error",
         "space-before-function-paren": "error",

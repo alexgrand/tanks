558dffcfd1d5fab31020eba90a1c56821a90f3b0
Alexrand <namelost@yandex.ru>
2018-03-31 14:24:07 +0800

    changed context for background canas element;

--- scripts/render.js
+++ scripts/render.js
@@ -1,10 +1,10 @@
 'use strict';
 (function () {
-	const ctx = window.renderCanvas.canvasBack.getContext('2d');
+	const ctxBack = window.renderCanvas.canvasBack.getContext('2d');
 
 	window.data.allObs.forEach((it) => {
 		if (it.name === 'border') {
-			ctx.fillRect(it.posX, it.posY, window.renderCanvas.blockSize, window.renderCanvas.blockSize);
+			ctxBack.fillRect(it.posX, it.posY, window.renderCanvas.blockSize, window.renderCanvas.blockSize);
 		}
 	});
 })();

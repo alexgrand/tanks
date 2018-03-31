b0d574f41c051a0054ab85514dfbd31f5990e068
Alexrand <namelost@yandex.ru>
2018-03-31 16:24:44 +0800

    added funcs for creating random map;

--- scripts/data.js
+++ scripts/data.js
@@ -8,6 +8,7 @@
 	const BLOCKS_AMOUNT = GAME_AREA / BLOCK_AREA;
 	const ROW_SIZE = GAME_WIDTH / BLOCK;
 	const GAME_ELEMENTS = new Map();
+	const BLOCK_SHAPES = new Map();
 	const ALL_OBSTALES = [];
 
 	class Obstacle {
@@ -47,18 +48,8 @@
 		}
 	}
 
-	const createGameElementsMap = function () {
-		const names = ['border', 'brick', 'wall', 'tree', 'npc', 'player'];
-
-		names.forEach((name) => {
-			if (name === 'npc' || name === 'player' || name === 'brick') {
-				GAME_ELEMENTS.set(name, {'destroyable': true, 'life': 3});
-			}
-			GAME_ELEMENTS.set(name, {'destroyable': false});
-		});
-	};
-
-	const checkIfBorder = function (index) {
+	const getRandomNumber = (max, min) => Math.floor(Math.random() * (max - min + 1)) + min;
+	const checkIfBorder = (index) => {
 		let isBorder = false;
 
 		if (index >= 0 && index <= ROW_SIZE) {
@@ -73,13 +64,35 @@
 
 		return isBorder;
 	};
+	const setGameElementsMap = () => {
+		const names = ['border', 'brick', 'wall', 'tree', 'npc', 'player'];
 
-	const createBlock = function (name, index) {
+		names.forEach((name) => {
+			if (name === 'npc' || name === 'player' || name === 'brick') {
+				GAME_ELEMENTS.set(name, {'destroyable': true, 'life': 3});
+			}
+			GAME_ELEMENTS.set(name, {'destroyable': false});
+		});
+	};
+
+	const setBlockShapesMap = () => {
+		BLOCK_SHAPES.set('shape1', [1, ROW_SIZE, ROW_SIZE + 1]);
+		BLOCK_SHAPES.set('shape2', [1, 2, 3]);
+		BLOCK_SHAPES.set('shape3', [ROW_SIZE, ROW_SIZE * 2, ROW_SIZE * 3]);
+	};
+	const createBlock = (name, index) => {
 		ALL_OBSTALES[index] = new Obstacle(name, index);
 		ALL_OBSTALES[index].findCoords();
 	};
-
-	const createMapBorders = function () {
+	const createBlockShape = (name, index, shape) => {
+		createBlock(name, index);
+		shape.forEach((it) => {
+			if (!ALL_OBSTALES[index + it]) {
+				createBlock(name, (index + it));
+			}
+		});
+	};
+	const createMapBorders = () => {
 		for (let i = 0; i < BLOCKS_AMOUNT; i++) {
 			if (!ALL_OBSTALES[i]) {
 				if (checkIfBorder(i)) {
@@ -88,9 +101,28 @@
 			}
 		}
 	};
+	const createAllObs = function () {
+		const blocks = window.gameSetup.amount;
 
-	createGameElementsMap();
+		for (const element in blocks) {
+			let amount = Math.floor(window.gameSetup.amount[element] * BLOCKS_AMOUNT);
+
+			while (amount) {
+				const randomIndex = getRandomNumber(BLOCKS_AMOUNT, 0);
+				const randomShape = BLOCK_SHAPES.get('shape1');
+
+				if (!ALL_OBSTALES[randomIndex]) {
+					createBlockShape(element, randomIndex, randomShape);
+					amount--;
+				}
+			}
+		}
+	};
+
+	setGameElementsMap();
+	setBlockShapesMap();
 	createMapBorders();
+	createAllObs();
 
 	window.data = {'allObs': ALL_OBSTALES};
 })();

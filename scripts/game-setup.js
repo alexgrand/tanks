e2525cbd0a6141a40860772b6409e29f44d94bfa
Alexrand <namelost@yandex.ru>
2018-03-31 16:20:35 +0800

    changed blcks size to 20;

--- scripts/game-setup.js
+++ scripts/game-setup.js
@@ -1,13 +1,12 @@
 'use strict';
 (function () {
 	window.gameSetup = {
-		'blockSize': 15,
+		'blockSize': 20,
 		'gameFinished': false,
 		'amount': {
-			'brick': 0.5,
-			'npcTank': 2,
-			'tree': 0.1,
-			'wall': 0.1
+			'brick': 0.05,
+			'tree': 0.02,
+			'wall': 0.02
 		}
 	};
 })();
